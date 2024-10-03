f2a85c4a96535c1ad780caa3c59b74e9a2e7f6dd
Ahmed Adel <119899462+Dolaaaa1@users.noreply.github.com>
2024-10-03 17:08:58 +0300

    Deploying to railway

--- project/settings.py
+++ project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-fn9d#%)m!!94af+stn8_&0x(bek=+7ldsy!4h54kwlazy6vrn$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
