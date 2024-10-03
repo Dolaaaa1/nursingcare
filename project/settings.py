7ca7ef53cd1e3f9bb76ab2796a8728d3a1804d3e
Ahmed Adel <119899462+Dolaaaa1@users.noreply.github.com>
2024-10-03 22:21:24 +0300

    Fix stitic

--- project/settings.py
+++ project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-fn9d#%)m!!94af+stn8_&0x(bek=+7ldsy!4h54kwlazy6vrn$"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
